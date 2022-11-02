359c883168fa1d23b7ed863fd8259eeb855eea04
Kamila Marchlewska <61703747+kamilamarchlewska@users.noreply.github.com>
2022-11-02 22:56:22 +0100

    JOIN, LISTAGG, SUBQUERIES, REGEXP
    JOIN, LISTAGG, SUBQUERIES, REGEXP

--- JOIN.sql
+++ JOIN.sql
@@ -72,7 +72,7 @@ WHERE
 
 /*---------------------------------------------------------------------------
  * 5. Write a SQL query to find those employees whose first name contains 
-   the letter ‘z’. Return first name, last name, department, city, 
+   the letter â€˜zâ€™. Return first name, last name, department, city, 
    and state province.
 */
 
@@ -138,7 +138,7 @@ FROM
     EMPLOYEES MNG
 WHERE
     MNG.EMPLOYEE_ID = EMP.MANAGER_ID;
-    
+ 
 
 
 /*---------------------------------------------------------------------------
@@ -228,7 +228,7 @@ LEFT JOIN JOB_HISTORY JOB_H ON EMP.EMPLOYEE_ID = JOB_H.EMPLOYEE_ID
 LEFT JOIN JOBS ON EMP.JOB_ID = JOBS.JOB_ID
 WHERE JOB_H.START_DATE > '93/01/01'
 AND JOB_H.END_DATE < '97/08/31';
-    
+
 
 
 /*---------------------------------------------------------------------------
@@ -267,9 +267,15 @@ GROUP BY
 /*---------------------------------------------------------------------------
  * 16. Write a SQL query to find the name of the country and departments, which are running there.
 */
+ 
+SELECT 
+    COUNTRY_NAME,
+    LISTAGG(DEPARTMENT_NAME, ', ') WITHIN GROUP (ORDER BY DEPARTMENT_NAME) AS DEP_NAME
+FROM DEPARTMENTS DEP 
+LEFT JOIN LOCATIONS LOC ON DEP.LOCATION_ID = LOC.LOCATION_ID
+LEFT JOIN COUNTRIES CNT ON LOC.COUNTRY_ID = CNT.COUNTRY_ID
+GROUP BY COUNTRY_NAME;
 
-
-    
 
 
 /*---------------------------------------------------------------------------
@@ -277,7 +283,12 @@ GROUP BY
    (first and last name) of the manager.
 */
 
-
+SELECT 
+    DISTINCT DEP.DEPARTMENT_NAME,
+    MNG.FIRST_NAME || ' ' || MNG.LAST_NAME AS MANAGER
+FROM DEPARTMENTS DEP
+LEFT JOIN EMPLOYEES EMP ON EMP.MANAGER_ID = DEP.MANAGER_ID
+LEFT JOIN EMPLOYEES MNG ON EMP.MANAGER_ID = MNG.EMPLOYEE_ID;
 
 
 
@@ -286,7 +297,12 @@ GROUP BY
   of employees for each job title.
 */
 
-
+SELECT 
+    JOB_TITLE,
+    CEIL(AVG(SALARY)) AS AVG_SALARY
+FROM EMPLOYEES EMP
+LEFT JOIN JOBS ON EMP.JOB_ID = JOBS.JOB_ID
+GROUP BY JOB_TITLE;
 
 
 
@@ -295,28 +311,55 @@ GROUP BY
    Return employee ID, starting date, end date, job ID and department ID.
 */
 
-
+SELECT 
+    EMP.EMPLOYEE_ID,
+    NVL(TO_CHAR(JOB_H.START_DATE), 'NO DATA') AS START_DATE_JOB,
+    NVL(TO_CHAR(JOB_H.END_DATE), 'NO DATA') AS END_DATE_JOB,
+    NVL(TO_CHAR(JOB_H.JOB_ID), 'NO DATA') AS ID_JOB,
+    NVL(TO_CHAR(JOB_H.DEPARTMENT_ID), 'NO DATA') AS ID_DEPARTMENT
+FROM EMPLOYEES EMP
+LEFT JOIN JOB_HISTORY JOB_H ON EMP.JOB_ID = JOB_H.JOB_ID
+WHERE EMP.SALARY > 1200;
 
 
 
 /*---------------------------------------------------------------------------
- * 20. write a SQL query to find out which departments have at least 
+ * 20. Write a SQL query to find out which departments have at least 
    two employees. Group the result set on country name and city. 
    Return country name, city, and number. 
 */
 
-
-
+SELECT 
+    COUNT(EMP.EMPLOYEE_ID) AS NUM_EMP,
+    CNT.COUNTRY_NAME,
+    LOC.CITY
+FROM EMPLOYEES EMP
+LEFT JOIN DEPARTMENTS DEP ON EMP.DEPARTMENT_ID = DEP.DEPARTMENT_ID
+LEFT JOIN LOCATIONS LOC ON DEP.LOCATION_ID = LOC.LOCATION_ID
+LEFT JOIN COUNTRIES CNT ON LOC.COUNTRY_ID = CNT.COUNTRY_ID
+WHERE
+    EMP.DEPARTMENT_ID IS NOT NULL
+GROUP BY
+    CNT.COUNTRY_NAME,
+    LOC.CITY
+HAVING 
+    COUNT(EMP.EMPLOYEE_ID) > 2;
 
 
 
 /*---------------------------------------------------------------------------
  * 21. Write a SQL query to find the department name, full name 
-   (first and last name) of the manager and their city.
+   (first and last name) of the manager and their city(department).
 */
 
-
-
+SELECT 
+    DISTINCT DEP.DEPARTMENT_NAME,
+    CITY,
+    MNG.FIRST_NAME || ' ' || MNG.LAST_NAME AS MANAGER
+FROM DEPARTMENTS DEP
+LEFT JOIN EMPLOYEES EMP ON EMP.MANAGER_ID = DEP.MANAGER_ID
+LEFT JOIN EMPLOYEES MNG ON EMP.MANAGER_ID = MNG.EMPLOYEE_ID
+LEFT JOIN LOCATIONS LOC ON LOC.LOCATION_ID = DEP.LOCATION_ID;
 
 
 
@@ -325,7 +368,13 @@ GROUP BY
    a department of ID 80. Return employee ID, job title, number of days worked.
 */
 
-
+SELECT
+    JOBS.JOB_TITLE,
+    JOB_H.EMPLOYEE_ID,
+    CEIL(JOB_H.END_DATE - JOB_H.START_DATE) AS DAYS_WORKED
+FROM JOBS
+LEFT JOIN JOB_HISTORY JOB_H ON JOBS.JOB_ID = JOB_H.JOB_ID
+WHERE JOB_H.DEPARTMENT_ID = 80;
 
 
 
@@ -334,7 +383,16 @@ GROUP BY
    of all employees working in any department in the city of London.
 */
 
-
+SELECT 
+    EMP.FIRST_NAME,
+    EMP.LAST_NAME,
+    EMP.SALARY,
+    LOC.CITY
+FROM EMPLOYEES EMP 
+LEFT JOIN DEPARTMENTS DEP ON EMP.DEPARTMENT_ID = DEP.DEPARTMENT_ID
+LEFT JOIN LOCATIONS LOC ON DEP.LOCATION_ID = LOC.LOCATION_ID
+WHERE 
+    REGEXP_LIKE(LOC.CITY, 'London');
 
 
 
@@ -343,7 +401,18 @@ GROUP BY
    start and end date of last jobs of employees who did not receive commissions.
 */
 
-
+SELECT
+    EMP.FIRST_NAME || ' ' || EMP.LAST_NAME,
+    JOBS.JOB_TITLE,
+    JOB_H.START_DATE,
+    JOB_H.END_DATE
+FROM EMPLOYEES EMP
+LEFT JOIN JOBS ON EMP.JOB_ID = JOBS.JOB_ID
+LEFT JOIN JOB_HISTORY JOB_H ON JOBS.JOB_ID = JOB_H.JOB_ID
+WHERE
+    COMMISSION_PCT IS NULL
+ORDER BY
+    EMP.FIRST_NAME || ' ' || EMP.LAST_NAME;
 
 
 
@@ -352,13 +421,30 @@ GROUP BY
    and number of employees in each department.  
 */
 
-
+SELECT
+    DEP.DEPARTMENT_ID,
+    DEP.DEPARTMENT_NAME,
+    COUNT(EMPLOYEE_ID) AS EMP_NUM
+FROM DEPARTMENTS DEP
+LEFT JOIN EMPLOYEES EMP ON DEP.DEPARTMENT_ID = EMP.DEPARTMENT_ID
+GROUP BY 
+    DEP.DEPARTMENT_ID, DEP.DEPARTMENT_NAME;
 
 
 
 /*---------------------------------------------------------------------------
  * 26. Write a SQL query to find out the full name (first and last name) 
-   of the employee with an ID and the name of the country where he/she 
+   of the employee with ID and the name of the country where he/she 
    is currently employed.
 */
+
+SELECT
+    FIRST_NAME || LAST_NAME AS EMP_NAME,
+    EMPLOYEE_ID,
+    COUNTRY_NAME
+FROM EMPLOYEES EMP 
+LEFT JOIN DEPARTMENTS DEP ON EMP.DEPARTMENT_ID = DEP.DEPARTMENT_ID
+LEFT JOIN LOCATIONS LOC ON DEP.LOCATION_ID = LOC.LOCATION_ID
+LEFT JOIN COUNTRIES CNT ON LOC.COUNTRY_ID = CNT.COUNTRY_ID;
+
 
